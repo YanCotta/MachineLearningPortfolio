c8183632b352abcf7c7b4936ab7e593efae4082a
Yan Cotta <yanpcotta@gmail.com>
2025-02-17 13:34:31 -0300

    Add setup.py for LSTM-based stock price prediction model with dependencies

---
+++ deep_learning/supervised/rnn/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="stock_price_predictor",
+    version="1.0.0",
+    author="Yan",
+    description="LSTM-based stock price prediction model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
