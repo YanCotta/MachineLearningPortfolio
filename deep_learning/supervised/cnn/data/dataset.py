8457f4e22c537364225f90b2f7f712866f482e86
Yan Cotta <yanpcotta@gmail.com>
2025-02-17 13:49:00 -0300

    Add dataset.py with download link for large dataset

---
+++ deep_learning/supervised/cnn/data/dataset.py
@@ -0,0 +1,6 @@
+
+
+
+# The dataset is quite large (over 10,000 pictures) and is not included in this repository, 
+# but I've added it to a Google Drive in order for you to download,
+# you can find it here: https://drive.google.com/file/d/1vtO7vdRndB0fCsz6RYCgH6DiYWLY7GUG/view?usp=sharing
